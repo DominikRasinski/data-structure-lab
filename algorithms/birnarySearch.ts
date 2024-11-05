eb3a7b26ee172c068a7c6cba5eae852a762de65c
Dominik Rasiński <dox.gmr@gmail.com>
2024-11-05 22:13:15 +0100

    Add some algoritms and array information

---
+++ algorithms/birnarySearch.ts
@@ -0,0 +1,32 @@
+/*
+* Input:   nums[] = [2, 3, 5, 7, 9] target = 7
+* Output: Element found at index 3
+* Input:   nums[] = [1, 4, 5, 8, 9] target = 2
+* Output: Element not found
+*/
+
+const firstNums: number[] = [2, 3, 0, 7, 9];
+const secondNums: number[] = [1, 4, 5, 8, 9];
+
+function binarySearch(target: number, array: number[]): number {
+    let start: number = 0;
+    let end: number = array.length - 1;
+    while (start <= end)
+    {
+        let mid: number = Math.ceil((start + end) / 2);
+        if(target == array[mid]) {
+            return mid;
+        }
+        if (target < array[mid]) {
+            end = mid - 1;
+        }
+        if (target > array[mid]) {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+
+// @ts-ignore
+console.log(binarySearch(7, firstNums));
+console.log(binarySearch(2, secondNums))
--- arrays/arraysParts.md
+++ arrays/arraysParts.md
@@ -1,1 +1,5 @@
 # Zaawansowane techniki, jakie możemy wykonywać na tablicy
+
+Najważniejszą częścią tablic jest to, że tablice są specjalnymi obiektami pozwalają na grupowanie dużej ilości danych w powiązanych z sobą komórkach pamięci.
+
+Przewagą tablic jest to, że są proste do odczytu oraz zapisu. Ale modyfikacja tablicy jest złożona ponieważ nie można od tak dodać wewnątrz niej nowego elementu, ponieważ będzie trzeba przestawić elementy przed i za nią
